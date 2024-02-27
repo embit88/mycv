4c78f13fbd64721489eacd6c43fb2ccd1def04e4
Embit88 <fyshtey@gmail.com>
2024-02-27 23:59:16 +0200

    remove cache folder deploy

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 
 const DB_DRIVER = 'mysql';
-const DB_HOST = '89.116.198.49';
-const DB_NAME = 'fushtei_com';
+const DB_HOST = 'localhost';
+const DB_NAME = 'vfx_test';
 const DB_CHARSET = 'utf8';
-const DB_USER = 'Embit';
-const DB_PASSWORD = 'GaptYc6p';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
