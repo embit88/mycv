df4f22280a38390ea939411ab8e322ed0924e881
embit88 <fyshtey@gmail.com>
2025-08-16 15:52:26 +0200

    add config

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -31,61 +31,44 @@ export default async function Home({params}: { params: Promise<{ locale: string 
                     <div className="col-span-4 sm:col-span-3">
                         <div className="bg-white shadow rounded-lg p-6">
                             <div className="flex flex-col items-center">
-                                {/*<img src="https://randomuser.me/api/portraits/men/94.jpg"*/}
-                                {/*     className="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0" alt=""/>*/}
+                                {appConfig.image ? (
+                                    <img src={appConfig.image}
+                                         className="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0" alt="" />
+                                ) : null}
                                 <h1 className="text-xl text-gray-700 uppercase font-bold tracking-wider">{translation.info.name}</h1>
                                 <p className="text-gray-600 text-sm m-2">{translation.info.profession}</p>
-                                <a href={`mailto:${translation.info.email}`}
-                                   className="text-gray-800 m-2">{translation.info.email}</a>
-                                <a href={`tel:${translation.info.phone_number}`}
-                                   className="text-gray-800 m-2">{translation.info.phone_number}</a>
-                                {/*<div className="flex justify-center items-center gap-5 mt-4">*/}
-                                {/*    <a className="text-gray-700 hover:text-orange-600"*/}
-                                {/*       aria-label="Visit TrendyMinds LinkedIn" href=""*/}
-                                {/*       target="_blank">*/}
-                                {/*        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="h-6">*/}
-                                {/*            <path fill="currentColor"*/}
-                                {/*                  d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z">*/}
-                                {/*            </path>*/}
-                                {/*        </svg>*/}
-                                {/*    </a>*/}
-                                {/*    <a className="text-gray-700 hover:text-orange-600"*/}
-                                {/*       aria-label="Visit TrendyMinds YouTube" href=""*/}
-                                {/*       target="_blank">*/}
-                                {/*        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="h-6">*/}
-                                {/*            <path fill="currentColor"*/}
-                                {/*                  d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z">*/}
-                                {/*            </path>*/}
-                                {/*        </svg>*/}
-                                {/*    </a>*/}
-                                {/*    <a className="text-gray-700 hover:text-orange-600"*/}
-                                {/*       aria-label="Visit TrendyMinds Facebook" href=""*/}
-                                {/*       target="_blank">*/}
-                                {/*        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className="h-6">*/}
-                                {/*            <path fill="currentColor"*/}
-                                {/*                  d="m279.14 288 14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z">*/}
-                                {/*            </path>*/}
-                                {/*        </svg>*/}
-                                {/*    </a>*/}
-                                {/*    <a className="text-gray-700 hover:text-orange-600"*/}
-                                {/*       aria-label="Visit TrendyMinds Instagram" href=""*/}
-                                {/*       target="_blank">*/}
-                                {/*        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="h-6">*/}
-                                {/*            <path fill="currentColor"*/}
-                                {/*                  d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z">*/}
-                                {/*            </path>*/}
-                                {/*        </svg>*/}
-                                {/*    </a>*/}
-                                {/*    <a className="text-gray-700 hover:text-orange-600"*/}
-                                {/*       aria-label="Visit TrendyMinds Twitter" href=""*/}
-                                {/*       target="_blank">*/}
-                                {/*        <svg className="h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">*/}
-                                {/*            <path fill="currentColor"*/}
-                                {/*                  d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z">*/}
-                                {/*            </path>*/}
-                                {/*        </svg>*/}
-                                {/*    </a>*/}
-                                {/*</div>*/}
+                                <a href={`mailto:${appConfig.email}`}
+                                   className="text-gray-800 m-2 underline">{appConfig.email}</a>
+                                <a href={`tel:${appConfig.phone}`}
+                                   className="text-gray-800 m-2 underline">{appConfig.phone}</a>
+                                <div className="flex justify-center items-center gap-5 mt-4">
+                                    <a className="text-gray-700 hover:opacity-80" href={appConfig.github} target="_blank">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16" fill="currentColor">
+                                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54
+                                                      2.29 6.53 5.47 7.59.4.07.55-.17.55-.38
+                                                      0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52
+                                                      -.01-.53.63-.01 1.08.58 1.23.82.72 1.21
+                                                      1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78
+                                                      -.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15
+                                                      -.08-.2-.36-1.02.08-2.12 0 0 .67-.21
+                                                      2.2.82a7.7 7.7 0 0 1 2-.27c.68 0
+                                                      1.36.09 2 .27 1.53-1.04 2.2-.82
+                                                      2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82
+                                                      1.27.82 2.15 0 3.07-1.87 3.75-3.65
+                                                      3.95.29.25.54.73.54 1.48 0 1.07-.01
+                                                      1.93-.01 2.2 0 .21.15.46.55.38A8.013
+                                                      8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                                        </svg>
+                                    </a>
+                                    <a className="text-gray-700 hover:opacity-80" href={appConfig.telegram} target="_blank">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 240 240">
+                                            <circle cx="120" cy="120" r="120" fill="#37aee2"/>
+                                            <path fill="#c8daea" d="M98 175c-3.94 0-3.28-1.49-4.65-5.24l-11.2-36.76 87.62-51.9"/>
+                                            <path fill="#a9c9dd" d="M98 175c3 0 4.32-1.38 6-3l16-15.55-19.96-12"/>
+                                            <path fill="#f6fbfe" d="M100 144.45 152.2 183c5.94 3.28 10.26 1.58 11.74-5.5l21.3-100.2c2.18-8.73-3.33-12.7-9.06-10.1L52 121.24c-8.6 3.45-8.55 8.23-1.57 10.37l31.24 9.74 72.4-45.6c3.42-2.07 6.55-.96 3.98 1.32"/>
+                                        </svg>
+                                    </a>
+                                </div>
                             </div>
                             <hr className="my-6 border-t border-gray-300"/>
                             <div className="flex flex-col text-center">
@@ -118,7 +101,6 @@ export default async function Home({params}: { params: Promise<{ locale: string 
                             </div>
                         </div>
                         <div className="bg-white shadow rounded-lg p-6">
-
                             <h2 className="text-xl font-bold mb-4">{translation.title.about_me}</h2>
                             <p className="text-gray-700">
                                 {translation.text.about_me}
--- appConfig.ts
+++ appConfig.ts
@@ -1,10 +1,15 @@
 const appConfig = {
     locales: [
         'en',
-        'ru',
         'de',
+        'ru',
     ],
     defaultLocale: 'en',
+    email: 'fyshtey@gmail.com',
+    phone: '+4915116529334',
+    github: 'https://github.com/embit88',
+    telegram: 'https://t.me/xprod',
+    image: '',
 };
 
 export default appConfig;
