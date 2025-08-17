123c15586493eede4ec3f9eb672b5627d5ff38cd
embit88 <fyshtey@gmail.com>
2025-08-17 14:21:42 +0200

    test deploy 4

--- run-prod.sh
+++ run-prod.sh
@@ -5,7 +5,7 @@ set -x
 COMPOSE_FILE=docker-compose.prod.yml
 
 echo "Останавливаем и удаляем контейнеры prod..."
-sudo docker compose -f "$COMPOSE_FILE" down
+docker compose -f "$COMPOSE_FILE" down
 
 echo "Собираем и запускаем prod контейнеры..."
-sudo docker compose -f "$COMPOSE_FILE" up -d --build
+docker compose -f "$COMPOSE_FILE" up -d --build
