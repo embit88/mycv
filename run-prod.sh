ac6c0377b76728a30dfc75a202850a45f6e45e1e
embit88 <fyshtey@gmail.com>
2025-08-18 10:45:44 +0200

    test dep6

--- run-prod.sh
+++ run-prod.sh
@@ -1,15 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 set -x
 
 COMPOSE_FILE=docker-compose.prod.yml
-DEPLOY_PATH=/var/www/vhosts/mymein.com/httpdocs
 
 echo "Останавливаем и удаляем контейнеры prod..."
 docker compose -f "$COMPOSE_FILE" down
-
-echo "Очищаем старые файлы на сервере (кроме .git если нужен)..."
-rm -rf $DEPLOY_PATH/!(.git)
 
 echo "Собираем и запускаем prod контейнеры..."
 docker compose -f "$COMPOSE_FILE" up -d --build
